327278ff5451d098b6be2470dd15787ac707c8a5
Rafael Kramer da Silva <rafael.silva@grupom8.local>
2024-01-05 18:06:10 -0300

    create the TodoService

--- src/pages/TodoPage/index.tsx
+++ src/pages/TodoPage/index.tsx
@@ -1,29 +1,31 @@
 import { useState } from 'react'
 import { AddTodoForm } from '../../components/AddTodoForm'
 import { TodoList } from '../../components/TodoList'
-import { Todo } from '../../models/TodoModel'
+import * as TodoService from './../../services/TodoService'
 import * as S from './styles'
+import { Todo } from '../../models/TodoModel'
 
 export function TodoPage() {
   const [todos, setTodos] = useState<Todo[]>([])
 
-  const handleAdd = (task: string) => {
-    const newTodo = { id: Date.now().toString(), task }
-    setTodos([...todos, newTodo])
+  const handleAddTodo = (task: string) => {
+    const updatedTodos = TodoService.addTodo(task)
+    setTodos(updatedTodos)
   }
 
-  const handleComplete = (id: string) => {
-    setTodos(todos.filter((todo) => todo.id !== id))
+  const handleRemoveTodo = (id: string) => {
+    const updatedTodos = TodoService.removeTodo(id)
+    setTodos(updatedTodos)
   }
 
   return (
     <S.TodoPage>
       <h1>Lista de Tarefas</h1>
-      <AddTodoForm onAdd={handleAdd} />
+      <AddTodoForm onAdd={handleAddTodo} />
       <TodoList
         todos={todos}
-        onComplete={handleComplete}
-        onRemove={handleComplete}
+        onComplete={() => ''}
+        onRemove={handleRemoveTodo}
       />
     </S.TodoPage>
   )
